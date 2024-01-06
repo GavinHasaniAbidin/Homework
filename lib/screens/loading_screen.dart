ffa175168658bb48148250da737aaf4bf8a3d28c
GavinHasaniAbidin <155839575+GavinHasaniAbidin@users.noreply.github.com>
2024-01-07 02:34:04 +0800

    Create loading_screen.dart

---
+++ lib/screens/loading_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class LoadingScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.max,
+          children: <Widget>[
+            Image.asset(
+              'assets/images/icon.png',
+              width: 72,
+            ),
+            Text(
+              "Habo - 養成習慣",
+              style: Theme.of(context).textTheme.headline5,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
